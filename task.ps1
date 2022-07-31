6f3f8f281b20c2702a7213cfb85dd0ee04bfea1c
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-01 02:48:40 +0800

    update,

--- task.ps1
+++ task.ps1
@@ -1,12 +1,14 @@
-cd cms
-  remove-item -Force -Recurse node_modules
-cd ..
-cd nextjs-client
-  remove-item -Force -Recurse node_modules
-cd ..
-cd truthy
-  remove-item -Force -Recurse node_modules
-cd ..
-cd truthy-react-frontend
-  remove-item -Force -Recurse node_modules
-cd ..
+# cd cms
+#   remove-item -Force -Recurse node_modules
+# cd ..
+# cd nextjs-client
+#   remove-item -Force -Recurse node_modules
+# cd ..
+# cd truthy
+#   remove-item -Force -Recurse node_modules
+# cd ..
+# cd truthy-react-frontend
+#   remove-item -Force -Recurse node_modules
+# cd ..
+
+yarn docker_dev up -d
