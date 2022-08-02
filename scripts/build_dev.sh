d5101c6b4df27928fe757f959b3c7a9f5456ed00
louiscklaw <louiscklaw@gmail.com>
2022-08-03 05:40:58 +0800

    update docker config,

--- scripts/build_dev.sh
+++ scripts/build_dev.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+set -x
+
 # docker compose -f docker-compose.dev.yml kill cms-app-dev
 # docker compose -f docker-compose.dev.yml rm -s -v -f cms-app-dev
 
-docker compose -f docker-compose.dev.yml up -d --build
+docker compose -f docker-compose.dev.yml up -d postgres pgweb redis
+
+sleep 5
+docker compose -f docker-compose.dev.yml up -d api
+
+sleep 5
+docker compose -f docker-compose.dev.yml up -d cms-app
+
+sleep 10
+docker compose -f docker-compose.dev.yml up -d proxy
 
 # docker compose -f docker-compose.dev.yml logs -f proxy
 docker compose -f docker-compose.dev.yml logs -f
--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-docker compose -f docker-compose.prod.yml kill cms-app-prod
-# docker compose -f docker-compose.prod.yml rm -s -v -f cms-app-prod
+set -x
 
-docker compose -f docker-compose.prod.yml up -d --build
+# docker compose -f docker-compose.dev.yml kill cms-app-dev
+# docker compose -f docker-compose.dev.yml rm -s -v -f cms-app-dev
 
-docker compose -f docker-compose.prod.yml logs -f cms-app-prod
-# docker compose -f docker-compose.prod.yml run -it cms-app-prod bash
+docker compose -f docker-compose.prod.yml up -d postgres pgweb redis
+
+sleep 5
+docker compose -f docker-compose.prod.yml up -d api
+
+sleep 5
+docker compose -f docker-compose.prod.yml up -d cms-app
+
+sleep 10
+docker compose -f docker-compose.prod.yml up -d proxy
+
+# docker compose -f docker-compose.prod.yml logs -f proxy
+docker compose -f docker-compose.prod.yml logs -f
