bbf74cc9589865ad60080851bb3e193bee28da21
louiscklaw <louiscklaw@gmail.com>
2022-08-03 15:08:27 +0800

    update test,

--- tests/helloworld/puppeteer/helloword.test.js
+++ tests/helloworld/puppeteer/helloword.test.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-test('adds 1 + 2 to equal 3', async () => {
+test('puppeteer helloworld', async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://example.com');
