0079aca3980ff4b22cfc565ec865690012a1ff80
louiscklaw <louiscklaw@gmail.com>
2022-08-05 04:27:24 +0800

    update,

--- cms-app/src/components/dashboard/overview/card-test-2.js
+++ cms-app/src/components/dashboard/overview/card-test-2.js
@@ -1,5 +1,6 @@
 import { Box, Button, Card, CardActions, Divider, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
 import { ArrowRight as ArrowRightIcon } from '../../../icons/arrow-right';
 import { Chart } from '../../chart';
 
@@ -9,46 +10,22 @@ const LineChart = () => {
   const chartOptions = {
     chart: {
       background: 'transparent',
-      toolbar: {
-        show: false,
-      },
-      zoom: {
-        enabled: false,
-      },
+      toolbar: { show: false },
+      zoom: { enabled: false },
     },
     colors: ['#2F3EB1'],
-    dataLabels: {
-      enabled: false,
-    },
-    fill: {
-      opacity: 1,
-    },
-    grid: {
-      show: false,
-    },
-    stroke: {
-      width: 3,
-    },
-    theme: {
-      mode: theme.palette.mode,
-    },
-    tooltip: {
-      enabled: false,
-    },
+    dataLabels: { enabled: false },
+    fill: { opacity: 1 },
+    grid: { show: false },
+    stroke: { width: 3 },
+    theme: { mode: theme.palette.mode },
+    tooltip: { enabled: false },
     xaxis: {
-      labels: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
+      labels: { show: false },
+      axisBorder: { show: false },
+      axisTicks: { show: false },
     },
-    yaxis: {
-      show: false,
-    },
+    yaxis: { show: false },
   };
 
   const chartSeries = [{ data: [0, 60, 30, 60, 0, 30, 10, 30, 0] }];
@@ -56,91 +33,34 @@ const LineChart = () => {
   return <Chart options={chartOptions} series={chartSeries} type="line" width={120} />;
 };
 
-const BarChart = () => {
-  const theme = useTheme();
+export const CardTest2 = () => {
+  const { t } = useTranslation();
 
-  const chartOptions = {
-    chart: {
-      background: 'transparent',
-      toolbar: {
-        show: false,
-      },
-      zoom: {
-        enabled: false,
-      },
-    },
-    colors: ['#2F3EB1'],
-    dataLabels: {
-      enabled: false,
-    },
-    fill: {
-      opacity: 1,
-    },
-    grid: {
-      show: false,
-    },
-    states: {
-      normal: {
-        filter: {
-          type: 'none',
-          value: 0,
-        },
-      },
-    },
-    stroke: {
-      width: 0,
-    },
-    theme: {
-      mode: theme.palette.mode,
-    },
-    tooltip: {
-      enabled: false,
-    },
-    xaxis: {
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
-      labels: {
-        show: false,
-      },
-    },
-    yaxis: {
-      show: false,
-    },
-  };
-
-  const chartSeries = [{ data: [10, 20, 30, 40, 50, 60, 5] }];
-
-  return <Chart options={chartOptions} series={chartSeries} type="bar" width={120} />;
+  return (
+    <Card>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'space-between',
+          px: 3,
+          py: 2,
+        }}
+      >
+        <div>
+          <Typography color="textSecondary" variant="body2">
+            {t('NUMBER_OF_RESTAURANTS')}
+          </Typography>
+          <Typography sx={{ mt: 1 }} variant="h5">
+            {1000}
+          </Typography>
+        </div>
+        <LineChart />
+      </Box>
+      <Divider />
+      <CardActions>
+        <Button endIcon={<ArrowRightIcon fontSize="small" />}>{t('SEE_ALL_VISITS')}</Button>
+      </CardActions>
+    </Card>
+  );
 };
-
-export const CardTest2 = () => (
-  <Card>
-    <Box
-      sx={{
-        alignItems: 'center',
-        display: 'flex',
-        justifyContent: 'space-between',
-        px: 3,
-        py: 2,
-      }}
-    >
-      <div>
-        <Typography color="textSecondary" variant="body2">
-          Impressions
-        </Typography>
-        <Typography sx={{ mt: 1 }} variant="h5">
-          1.9M
-        </Typography>
-      </div>
-      <LineChart />
-    </Box>
-    <Divider />
-    <CardActions>
-      <Button endIcon={<ArrowRightIcon fontSize="small" />}>See all visits</Button>
-    </CardActions>
-  </Card>
-);
