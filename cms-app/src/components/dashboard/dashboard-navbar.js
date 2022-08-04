0393b99a007ee03cf6f277a7690f32759423b48c
louiscklaw <louiscklaw@gmail.com>
2022-08-04 19:09:27 +0800

    update,

--- cms-app/src/components/dashboard/dashboard-navbar.js
+++ cms-app/src/components/dashboard/dashboard-navbar.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { AppBar, Avatar, Badge, Box, ButtonBase, IconButton, Toolbar, Tooltip } from '@mui/material';
+import { AppBar, Avatar, Badge, Box, ButtonBase, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Menu as MenuIcon } from '../../icons/menu';
 import { Bell as BellIcon } from '../../icons/bell';
@@ -162,7 +162,7 @@ const AccountButton = () => {
   // To get the user from the authContext, you can use
   // `const { user } = useAuth();`
   const user = {
-    avatar: '/static/mock-images/avatars/avatar-anika_visser.png',
+    avatar: '/cms/static/mock-images/avatars/avatar-anika_visser.png',
     name: 'Anika Visser',
   };
 
@@ -180,19 +180,9 @@ const AccountButton = () => {
         component={ButtonBase}
         onClick={handleOpenPopover}
         ref={anchorRef}
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          ml: 2,
-        }}
+        sx={{ alignItems: 'center', display: 'flex', ml: 2 }}
       >
-        <Avatar
-          sx={{
-            height: 40,
-            width: 40,
-          }}
-          src={user.avatar}
-        >
+        <Avatar sx={{ height: 40, width: 40 }} src={user.avatar}>
           <UserCircleIcon fontSize="small" />
         </Avatar>
       </Box>
@@ -206,37 +196,16 @@ export const DashboardNavbar = props => {
 
   return (
     <>
-      <DashboardNavbarRoot
-        sx={{
-          left: {
-            lg: 280,
-          },
-          width: {
-            lg: 'calc(100% - 280px)',
-          },
-        }}
-        {...other}
-      >
-        <Toolbar
-          disableGutters
-          sx={{
-            minHeight: 64,
-            left: 0,
-            px: 2,
-          }}
-        >
-          <IconButton
-            onClick={onOpenSidebar}
-            sx={{
-              display: {
-                xs: 'inline-flex',
-                lg: 'none',
-              },
-            }}
-          >
+      <DashboardNavbarRoot sx={{ left: { lg: 280 }, width: { lg: 'calc(100% - 280px)' } }} {...other}>
+        <Toolbar disableGutters sx={{ minHeight: 64, left: 0, px: 2 }}>
+          <IconButton onClick={onOpenSidebar} sx={{ display: { xs: 'inline-flex', lg: 'none' } }}>
             <MenuIcon fontSize="small" />
           </IconButton>
           <Box sx={{ flexGrow: 1 }} />
+          <Box>
+            <Typography>development</Typography>
+          </Box>
+
           <LanguageButton />
           <ContentSearchButton />
           <ContactsButton />
