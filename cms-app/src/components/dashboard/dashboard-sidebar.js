b804ba671f9c1347fa01631884b0ce1b5891d44a
louiscklaw <louiscklaw@gmail.com>
2022-08-05 01:36:52 +0800

    update foratting,

--- cms-app/src/components/dashboard/dashboard-sidebar.js
+++ cms-app/src/components/dashboard/dashboard-sidebar.js
@@ -31,26 +31,25 @@ import { Scrollbar } from '../scrollbar';
 import { DashboardSidebarSection } from './dashboard-sidebar-section';
 import { OrganizationPopover } from './organization-popover';
 
+const NewChip = () => {
+  return (
+    <Chip
+      color="secondary"
+      label={<Typography sx={{ fontSize: '10px', fontWeight: '600' }}>NEW</Typography>}
+      size="small"
+    />
+  );
+};
+
 const getSections = t => [
   {
     title: t('General'),
     items: [
       { title: t('Overview'), path: '/dashboard', icon: <HomeIcon fontSize="small" /> },
       { title: t('Analytics'), path: '/dashboard/analytics', icon: <ChartBarIcon fontSize="small" /> },
-      { title: t('Finance'), path: '/dashboard/finance', icon: <ChartPieIcon fontSize="small" /> },
-      {
-        title: t('Logistics'),
-        path: '/dashboard/logistics',
-        icon: <TruckIcon fontSize="small" />,
-        chip: (
-          <Chip
-            color="secondary"
-            label={<Typography sx={{ fontSize: '10px', fontWeight: '600' }}>NEW</Typography>}
-            size="small"
-          />
-        ),
-      },
-      { title: t('Account'), path: '/dashboard/account', icon: <UserCircleIcon fontSize="small" /> },
+      // { title: t('Finance'), path: '/dashboard/finance', icon: <ChartPieIcon fontSize="small" /> },
+      // { title: t('Logistics'), path: '/dashboard/logistics', icon: <TruckIcon fontSize="small" />, chip: <NewChip /> },
+      // { title: t('Account'), path: '/dashboard/account', icon: <UserCircleIcon fontSize="small" /> },
     ],
   },
   {
