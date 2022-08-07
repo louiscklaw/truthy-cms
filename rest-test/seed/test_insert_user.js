d5da115cb18876af0f153ec3fcb4e7f511be9128
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-07 18:34:17 +0800

    update test insert user,

--- rest-test/seed/test_insert_user.js
+++ rest-test/seed/test_insert_user.js
@@ -1,16 +1,17 @@
 'use strict';
 import fetch, { FormData, Blob, blobFrom, blobFromSync, File, fileFrom, fileFromSync } from 'node-fetch';
 
+import config from './config.js';
 import login from './login.js';
 import logout from './logout.js';
 
 var auth_cookie = await login();
 
-await fetch('http://test.docker.localhost/api/user', {
+await fetch(`${config.API_ENDPOINT}/user`, {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
-    name: 'restaurant 1',
+    name: 'user 1',
     location: 'hong kong',
     isActive: true,
   }),
