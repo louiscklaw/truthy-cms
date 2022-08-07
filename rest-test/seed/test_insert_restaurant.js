25462c54ccdb724099139e172c5f42cf2063a0ae
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-07 18:35:26 +0800

    update insert restaurant,

--- rest-test/seed/test_insert_restaurant.js
+++ rest-test/seed/test_insert_restaurant.js
@@ -1,12 +1,13 @@
 'use strict';
 import fetch, { FormData, Blob, blobFrom, blobFromSync, File, fileFrom, fileFromSync } from 'node-fetch';
 
+import config from './config.js';
 import login from './login.js';
 import logout from './logout.js';
 
 var auth_cookie = await login();
 
-await fetch('http://test.docker.localhost/api/restaurants', {
+await fetch(`${config.API_ENDPOINT}/restaurants`, {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
