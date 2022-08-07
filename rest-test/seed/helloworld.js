90066291e5923537c2617d7d59ab2ef9bbb3c26f
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-07 18:23:23 +0800

    update helloworld api test,

--- rest-test/seed/helloworld.js
+++ rest-test/seed/helloworld.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import fetch, { FormData, Blob, blobFrom, blobFromSync, File, fileFrom, fileFromSync } from 'node-fetch';
+import fetch from 'node-fetch';
 
-const params = new URLSearchParams();
-params.append('username', 'admin');
-params.append('password', 'superSecure');
+import config from './config.js';
 
-var response = await fetch('http://localhost:7777/api', {
+var response = await fetch(config.API_ENDPOINT, {
   method: 'GET',
 });
 
 var res_json = await response.json();
+
+console.assert(res_json?.message, 'api helloworld failed');
