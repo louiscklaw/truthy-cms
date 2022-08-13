9e4e1c6c23a48d31775b39c34ca91dddd3ebe390
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-13 13:37:55 +0800

    update  working bull-board,

--- truthy/src/main.ts
+++ truthy/src/main.ts
@@ -7,7 +7,17 @@ import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/sw
 import * as config from 'config';
 import * as cookieParser from 'cookie-parser';
 
+import * as Bull from 'bull';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { createBullBoard } from '@bull-board/api';
+import { ExpressAdapter } from '@bull-board/express';
+
 import { AppModule } from 'src/app.module';
+
+const connectionOpts = {
+  host: process.env.REDIS_HOST || 'localhost',
+  port: parseInt(process.env.REDIS_PORT) || 6379,
+};
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
@@ -18,6 +28,16 @@ async function bootstrap() {
 
   // baseurl
   app.setGlobalPrefix('api');
+
+  // bull-board
+  // http://localhost:7777/api/bull-board/queue/helloworld_queue
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/api/bull-board');
+  const aQueue = new Bull('helloworld_queue', { redis: { ...connectionOpts } });
+  createBullBoard({ queues: [new BullAdapter(aQueue)], serverAdapter });
+  app.use('/api/bull-board', serverAdapter.getRouter());
+  logger.log({ redis_config: connectionOpts });
+  // bull-board
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors({
@@ -54,7 +74,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
 
   app.use(cookieParser());
+
   await app.listen(port);
+
   logger.log(`Application listening in port: ${port}`);
 }
 
