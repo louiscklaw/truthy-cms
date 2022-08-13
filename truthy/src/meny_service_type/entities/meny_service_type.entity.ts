bd58df813ea9dbca9f87a6df97090d28c061434d
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-13 20:25:46 +0800

    update,

--- truthy/src/meny_service_type/entities/meny_service_type.entity.ts
+++ truthy/src/meny_service_type/entities/meny_service_type.entity.ts
@@ -1,7 +1,17 @@
 import { UserEntity } from 'src/auth/entity/user.entity';
 import { CustomBaseEntity } from 'src/common/entity/custom-base.entity';
 import { RestaurantEntity } from 'src/restaurants/entities/restaurant.entity';
-import { Column, Entity, Generated, Index, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  Index,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'meny_service_type' })
 export class MenyServiceTypeEntity extends CustomBaseEntity {
@@ -29,6 +39,12 @@ export class MenyServiceTypeEntity extends CustomBaseEntity {
 
   @Column({ default: false })
   canProcessTakeAwayOrder: boolean;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @ManyToMany(() => UserEntity, user => user.meny_service_type)
