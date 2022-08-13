d6c94f21ceb05053c26e18f31f78149566c9fae8
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-13 20:22:04 +0800

    update,

--- truthy/src/access_log/access_log.module.ts
+++ truthy/src/access_log/access_log.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccessLogService } from './access_log.service';
 import { AccessLogController } from './access_log.controller';
+import { AccessLog } from './entities/access_log.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AccessLog])],
   controllers: [AccessLogController],
   providers: [AccessLogService],
 })
