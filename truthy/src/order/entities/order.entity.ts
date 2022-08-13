0df3bf564df7f8105c14b86b4bb3110128fdb61b
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-13 20:26:20 +0800

    update,

--- truthy/src/order/entities/order.entity.ts
+++ truthy/src/order/entities/order.entity.ts
@@ -1,1 +1,28 @@
-export class Order {}
+import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Generated('uuid')
+  uuid: string;
+
+  @Column()
+  name: string;
+
+  @Column({ default: '' })
+  description: string;
+
+  @Column({ default: '' })
+  remarks: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
